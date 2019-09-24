2f9df6351e945fad2709d43d384ba7303e107592
Neko-bot-jpg <coco86973@gmail.com>
2019-09-24 16:51:06 +0200

    uptime

--- main.js
+++ main.js
@@ -12,37 +12,75 @@ client.on("message", msg => {
 
 client.login(TOKEN);
 
-client.on('message', message => {
-
+client.on("message", message => {
   if (!message.guild) return;
 
-
-  if (message.content.startsWith('!kick')) {
-
+  if (message.content.startsWith("!kick")) {
     const user = message.mentions.users.first();
 
     if (user) {
-
       const member = message.guild.member(user);
 
       if (member) {
+        member
+          .kick("Optional reason that will display in the audit logs")
+          .then(() => {
+            message.reply(`Coup de pied réussi ${user.tag}`);
+          })
+          .catch(err => {
+            message.reply("Je suis incapable de frapper le membre");
 
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-
-          message.reply(`Successfully kicked ${user.tag}`);
-        }).catch(err => {
-
-          message.reply('I was unable to kick the member');
-
-          console.error(err);
-        });
+            console.error(err);
+          });
       } else {
-
-        message.reply('That user isn\'t in this guild!');
+        message.reply("That user isn't in this guild!");
       }
-
     } else {
-      message.reply('You didn\'t mention the user to kick!');
+      message.reply("Il n'y as aucun mention pour kick !");
     }
   }
 });
+
+const Discord = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+  let cMS = convertMS(client.uptime);
+  let uptime =
+    cMS.d +
+    " days : " +
+    cMS.h +
+    " hours : " +
+    cMS.m +
+    " minutes : " +
+    cMS.s +
+    " seconds";
+
+  const uptimeEmbed = new Discord.RichEmbed()
+    .setColor(`#f395ea`)
+    .addField(`Le bot est en ligne depuis`, `${uptime}`)
+    .setFooter(">>Footer<<");
+
+  message.channel.send(uptimeEmbed);
+  message.delete();
+};
+
+function convertMS(ms) {
+  var d, h, m, s;
+  s = Math.floor(ms / 1000);
+  m = Math.floor(s / 60);
+  s = s % 60;
+  h = Math.floor(m / 60);
+  m = m % 60;
+  d = Math.floor(h / 24);
+  h = h % 24;
+  return {
+    d: d,
+    h: h,
+    m: m,
+    s: s
+  };
+}
+
+module.exports.help = {
+  name: "uptime",
+};
